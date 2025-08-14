b009fe1157c93284a2fc6335d455cd631b8f35d9
Ikaterin-crtl <catarinaneedsajob@gmail.com>
2025-08-14 16:54:30 -0300

    commit carroussel

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const swiper = new Swiper('.swiper-container', {
+  const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
     slidesPerView: 1,
